aff1e7e75fce6c46381291e195074ec91d047e83
Carlos <Carlos>
2016-01-26 12:25:46 -0300

    avances de video y tamaño del titulo

--- js/cmScript.js
+++ js/cmScript.js
@@ -2,6 +2,9 @@ $(function(){
 	var $headerCm = $('#cm-header');
 	var $topHeader = $headerCm.offset().top;
 	var $btnScroll = $('.circle');
+	var $video = $(".tapa");
+	var pantalla = $(window).width();
+	var $titulo = $("#titulo");
 	$(window).on('scroll',function(){
 		var $top = $(window).scrollTop();
 		if($top > $topHeader){
@@ -11,6 +14,20 @@ $(function(){
 		}
 	});
 	$btnScroll.on('click',function(){
-		 $('body , html').animate({scrollTop:$topHeader},1000);
+		 $('body , html').animate({scrollTop:$topHeader},1000,'easeInOutExpo');
 	});
+	$video.on("click",function(){
+		var youtube = $(this).siblings("iframe");
+		$(youtube).attr("src","https://www.youtube.com/embed/-shCDK2nPxU?autoplay=1&rel=0&controls=0&showinfo=0&modestbranding=0");
+		var myVar = setInterval(function(){ $video.css("display","none") }, 1500);
+		
+	});
+
+	if(pantalla < 480){
+		$titulo.css("font-size","3.5em");
+	}
+	if (pantalla > 480 && pantalla < 769){
+		$titulo.css("font-size","4.8em");
+	}
+
 });
